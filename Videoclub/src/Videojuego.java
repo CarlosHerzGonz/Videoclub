92c44faeb002320e943958afe2d03c1330a3f1e4
Alvaro <a.garciap@salesianospizarrales.educamos.com>
2022-05-05 17:33:19 +0200

    Commit clase Videojuego

---
+++ Videoclub/src/Videojuego.java
@@ -0,0 +1,75 @@
+public class Videojuego {
+    private String titulo,genero,compañia;
+    private int horasE;
+    private boolean entregado;
+
+    Videojuego(){
+        this.horasE =10;
+        this.entregado = false;
+        this.titulo = "titulo";
+        this.genero = "genero";
+        this.compañia = "compañia";
+    }
+
+    Videojuego(String tit, int hor){
+        this.horasE = hor;
+        this.entregado = false;
+        this.titulo = tit;
+        this.genero = "genero";
+        this.compañia = "compañia";
+    }
+
+    Videojuego(String tit, int hor, String gen, String comp){
+        this.horasE = hor;
+        this.entregado = false;
+        this.titulo = tit;
+        this.genero = gen;
+        this.compañia = comp;
+    }
+
+    public String getCompañia() {
+        return compañia;
+    }
+
+    public void setCompañia(String compañia) {
+        this.compañia = compañia;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getHorasE() {
+        return horasE;
+    }
+
+    public void setHorasE(int horasE) {
+        this.horasE = horasE;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+
+
+    
+
+    
+
+
+    
+}
